962e6095bc97ea4cc4ab87e5e8f51635a1443f31
Sylvain Leroux <sylvain@chicoree.fr>
2023-02-01 14:12:48 +0100

    Implement root-finding for continuous monotonic functions

--- fin/math.py
+++ fin/math.py
@@ -1,1 +1,51 @@
 EPSILON=0.0001
+
+class MathError(Exception):
+    pass
+
+class DomainError(MathError):
+    pass
+
+def solve(fct, x, min, max, vars={}):
+    """
+    Find a root for a (supposedly) continuous and monotonic function in the
+    range [min, max].
+    """
+    # Sanityze
+    if max < min:
+        min, max = max, min
+
+    da = vars.copy()
+    db = vars.copy()
+    dc = vars.copy()
+
+    da[x] = min
+    db[x] = (min+max)/2
+    dc[x] = max
+
+    a = fct(**da)
+    c = fct(**dc)
+    if a*c > 0:
+        raise DomainError()
+
+    while not -EPSILON < min-max < EPSILON:
+        db[x] = (min+max)/2
+        b = fct(**db)
+        if b*c < 0:
+            a = b;
+            min = da[x] = db[x]
+        elif a*b < 0:
+            c = b
+            max = dc[x] = db[x]
+        # Special cases
+        elif a == 0:
+            return da[x]
+        elif b == 0:
+            return db[x]
+        elif c == 0:
+            return dc[x]
+        else:
+            raise DomainError()
+
+    return db[x]
+
--- tests/fin/test_math.py
+++ tests/fin/test_math.py
@@ -5,3 +5,18 @@ import fin.math
 class TestMathConstants(unittest.TestCase):
     def test_epsilon_defined(self):
         self.assertEqual(fin.math.EPSILON, 0.0001)
+
+class TestMathRoot(unittest.TestCase):
+    def test_root_lin1(self):
+        def f(x):
+            return 2*x+3 # root is -1.5
+
+        actual = fin.math.solve(f, 'x', -10, +10)
+        self.assertAlmostEqual(actual, -1.5, delta=fin.math.EPSILON)
+
+    def test_root_quad1(self):
+        def f(x):
+            return 2*(x-1)*(x-1)-8 # roots are -1 and 3
+
+        actual = fin.math.solve(f, 'x', 0, +10)
+        self.assertAlmostEqual(actual, 3, delta=fin.math.EPSILON)
