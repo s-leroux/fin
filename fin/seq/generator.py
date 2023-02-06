17ed7cd32d5a619d529ede305c5188ab54430efb
Sylvain Leroux <sylvain@chicoree.fr>
2023-02-06 12:51:04 +0100

    Add the rawdata generator

--- fin/seq/generator.py
+++ fin/seq/generator.py
@@ -1,7 +1,21 @@
+# ======================================================================
+# Constants and utilities
+# ======================================================================
+def samples(values=()):
+    return tuple(values)
+    # return array('f', values)
 
 # ======================================================================
-# Constant generators
+# Core generators
 # ======================================================================
+def nothing():
+    """ Empty generator. Produce an infinite stream of nothingness.
+    """
+    def _nothing(count, cache=()*1000):
+        return cache[:count], _nothing
+
+    return _nothing
+
 def constant(*values):
     """ Return a generator producing an infinite stream of samples
         with the same value
@@ -12,3 +26,20 @@ def constant(*values):
         return buffer[:n], read
 
     return read
+
+def rawdata(data, cont=nothing()):
+    """ Return a generator from rawdata.
+    """
+    data = samples(data)
+    def at(offset):
+        def read(count):
+            stop = offset+count
+            result = data[offset:stop]
+            if result:
+                return result, at(stop)
+            else:
+                return cont(count)
+
+        return read
+
+    return at(0)
--- tests/fin/seq/test_generator.py
+++ tests/fin/seq/test_generator.py
@@ -2,12 +2,38 @@ import unittest
 
 import fin.seq.generator as generator
 
-class TestContant(unittest.TestCase):
+class TestUtils(unittest.TestCase):
+    def test_samples(self):
+        l = [(1,2), (3,4)]
+        expected = tuple(l)
+        actual = generator.samples(l)
+
+        self.assertEqual(actual, expected)
+
+class TestConstant(unittest.TestCase):
     def test_const_value(self):
         g = generator.constant(1)
         for n in 5,6,7:
             actual, g = g(n)
             self.assertSequenceEqual(actual, ((1,),)*n)
+
+    def test_const_values(self):
+        g = generator.constant(1,2,3)
+        for n in 5,6,7:
+            actual, g = g(n)
+            self.assertSequenceEqual(actual, ((1,2,3,),)*n)
+
+class TestRawData(unittest.TestCase):
+    def test_data(self):
+        data = ((1,2),(3,4),(5,6),(7,8))
+        g = generator.rawdata(data)
+        result = []
+
+        for i in range(1,6):
+            chunk, g = g(i)
+            result.extend(chunk)
+
+        self.assertSequenceEqual(tuple(result), data)
 
     def test_const_values(self):
         g = generator.constant(1,2,3)
