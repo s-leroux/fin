80e2abdfd8dfe7534e6ba1f07c4f6f3a09ce9a47
Sylvain Leroux <sylvain@chicoree.fr>
2023-02-16 15:08:43 +0100

    Implement the Black-Scholes model for option pricing

---
+++ fin/model/option.py
@@ -0,0 +1,72 @@
+import fin.model
+
+import math
+import fin.math
+
+class Call(fin.model.Model):
+    def __init__(self, params):
+        pnames = (
+            "k",
+            "t",
+            "s_0",
+            "sigma_0",
+            "r_0",
+            "c_0",
+        )
+        super().__init__(call, pnames, params)
+
+class Put(fin.model.Model):
+    def __init__(self, params):
+        pnames = (
+            "k",
+            "t",
+            "s_0",
+            "sigma_0",
+            "r_0",
+            "p_0",
+        )
+        super().__init__(put, pnames, params)
+
+def call(k, t, s_0, sigma_0, r_0, c_0, sqrt=math.sqrt, log=math.log, exp=math.exp, cdf=fin.math.cdf):
+    """
+    Model function using the Black-Scholes formula to price call options
+
+    This function evaluates to 0 if the model is at equilibrium given:
+    - k: Strike price
+    - t: Time until expiration
+    - s_0: Underlying asset price on the valuation date
+    - sigma_0: Underlying asset volatility on the valuation date
+    - r_0: Continuously compounded risk-free interest rate on the valuation date
+    - c_0: Long call value
+    """
+    v = sigma_0*sqrt(t)
+    d1 = (log(s_0/k)+(r_0+sigma_0*sigma_0/2)*t)
+    try:
+        d1 /= v
+    except ZeroDivisionError:
+        d1 *= float("inf")
+    d2 = d1 - v
+
+    return c_0-(s_0*cdf(d1)-k*exp(-r_0*t)*cdf(d2))
+
+def put(k, t, s_0, sigma_0, r_0, p_0, sqrt=math.sqrt, log=math.log, exp=math.exp, cdf=fin.math.cdf):
+    """
+    Model function using the Black-Scholes formula to price put options
+
+    This function evaluates to 0 if the model is at equilibrium given:
+    - k: Strike price
+    - t: Time until expiration
+    - s_0: Underlying asset price on the valuation date
+    - sigma_0: Underlying asset volatility on the valuation date
+    - r_0: Continuously compounded risk-free interest rate on the valuation date
+    - p_0: Long put value
+    """
+    v = sigma_0*sqrt(t)
+    d1 = (log(s_0/k)+(r_0+sigma_0*sigma_0/2)*t)
+    try:
+        d1 /= v
+    except ZeroDivisionError:
+        d1 *= float("inf")
+    d2 = d1 - v
+
+    return p_0+(s_0*cdf(-d1)-k*exp(-r_0*t)*cdf(-d2))
---
+++ tests/fin/model/test_option.py
@@ -0,0 +1,70 @@
+import unittest
+
+import fin.model.option as option
+import fin.math
+
+class TestModelOption(unittest.TestCase):
+    def test_option_call_pricing(self):
+        """ Test call option pricing using the Black-Scholes model
+
+            From "Derivatives Essentials: An Introduction to Forwards, Futures, Options, and Swaps"
+            by Aron Gottesman, p96
+        """
+        STRIKE=800.0
+        MATURITY=1
+        RISK_FREE_RATE=0.05
+        ASSET_PRICE=1000.0
+        ASSET_VOLATILITY=0.25
+
+        model = option.Call(dict(
+            k=STRIKE,
+            t=MATURITY,
+            s_0=ASSET_PRICE,
+            sigma_0=ASSET_VOLATILITY,
+            r_0=RISK_FREE_RATE,
+        ))
+        self.assertAlmostEqual(model.c_0, 254.13, places=2)
+
+    def test_option_put_pricing(self):
+        """ Test put option pricing using the Black-Scholes model
+
+            From "Derivatives Essentials: An Introduction to Forwards, Futures, Options, and Swaps"
+            by Aron Gottesman, p98
+        """
+        STRIKE=95
+        MATURITY=0.25
+        RISK_FREE_RATE=0.02
+        ASSET_PRICE=85.0
+        ASSET_VOLATILITY=0.65
+
+        model = option.Put(dict(
+            k=STRIKE,
+            t=MATURITY,
+            s_0=ASSET_PRICE,
+            sigma_0=ASSET_VOLATILITY,
+            r_0=RISK_FREE_RATE,
+        ))
+        self.assertAlmostEqual(model.p_0, 16.96, places=2)
+
+    def test_option_implied_volatility_from_call(self):
+        """ Test implied volatility's derifation from Black-Scholes and a call option
+
+            From "Options, Furtures and Other Derivatives, 5th edition"
+            by John C. Hull, p250
+        """
+        CALL_PRICE=1.875
+        STRIKE=20.0
+        MATURITY=0.25
+        RISK_FREE_RATE=0.10
+        ASSET_PRICE=21.0
+
+        model = option.Call(dict(
+            c_0=CALL_PRICE,
+            k=STRIKE,
+            t=MATURITY,
+            s_0=ASSET_PRICE,
+            r_0=RISK_FREE_RATE,
+        ))
+        self.assertAlmostEqual(model.sigma_0, 0.235, places=3)
+
+
