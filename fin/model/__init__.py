2b581676ab4418ababa0db4d717d7b32eaeb69bc
Sylvain Leroux <sylvain@chicoree.fr>
2023-02-18 15:17:55 +0100

    Allow a custom solver in models

--- fin/model/__init__.py
+++ fin/model/__init__.py
@@ -1,62 +1,84 @@
 import fin.math
 
+# ======================================================================
+# Exceptions
+# ======================================================================
 class Underdefined(Exception):
-    def __init__(self, params, *args):
-        message = "Too many missing parameters: {}".format(params)
+    def __init__(self, expected, actual, *args):
+        message = "Too many missing parameters:\nExpected: {}\nActual: {}".format(expected.keys(), actual)
         super(Exception, self).__init__(message)
 
-        self._params = params
+        self._actual = actual
+        self._expected = expected
 
+# ======================================================================
+# Model
+# ======================================================================
 class Model:
-    def __init__(self, fct, pnames, params):
-        a = -fin.math.HUGE
-        b = fin.math.HUGE
+    """
+    A mathematical model whose parameters are interdependant.
 
-        self._fct = fct
+    The model is defined by an ``equilibrium function``. That is a function 
+    which returns 0 if the model is at equilibrium.
+
+    You can query the individual parameter values using the subscript syntax.
+    When a parameter was not set, the model first try to infer its values
+    from a user specified function or using the generic solver.
+    """
+    def __init__(self, eq, params, values):
+        # For compatibility with previous implementations
+        if type(params) == tuple:
+            params = {k: None for k in params}
+
+        def solver(pname):
+            def _solver(**params):
+                # Default resolution forwarded to the generic solver
+                return fin.math.solve(eq,
+                            pname,
+                            -fin.math.HUGE,
+                            fin.math.HUGE,
+                            params
+                       )
+            return _solver
+
+        if len(params) - len(values) > 1:
+            raise Underdefined(params, values)
+
+        self._eq = eq
+        self._values = {}
         self._params = {}
-        missing = []
-        for pname in pnames:
-            pvalue = self._params[pname] = params.get(pname)
-            if pvalue is None:
-                missing.append(pname)
+
+        for pname, pvalue in params.items():
+            if callable(pvalue):
+                self._params[pname] = pvalue
             else:
-                try:
-                    a, b = pvalue
-                    missing.append(pname)
-                except:
-                    pass
+                self._params[pname] = solver(pname)
 
-        if len(missing) > 1:
-            raise Underdefined(missing)
-
-        if len(missing) == 1:
-            pname = missing[0]
-            self._params[pname] = fin.math.solve(
-                    self._fct,
-                    pname,
-                    a,
-                    b,
-                    self._params
-            )
+        self._values = values.copy()
 
     def adjust(self, x, conditions):
         """
         Recalculate the "x" parameter under the given conditions.
         Return a new instance of the receiver's class.
         """
-        new_params = self._params.copy()
-        new_params.update(conditions)
-        new_params.pop(x, None)
+        new_values = { k: self[k] for k in self._params.keys() if k != x and k not in conditions }
+        new_values.update(conditions);
 
-        return self.clone(new_params)
+        return self.clone(new_values)
 
-    def clone(self, new_params):
-        return type(self)(new_params)
+    def clone(self, new_values):
+        return type(self)(new_values)
 
     def __getitem__(self, name):
-        return self._params[name]
+        sentinel = object()
+        value = self._values.get(name, sentinel)
+        if value is sentinel:
+            # Not found!
+            value = self._values[name] = self._params[name](**self._values)
+
+        return value
 
     def __repr__(self):
         T = type(self)
-        return "{}.{}({})".format(T.__module__, T.__qualname__, self._params)
+        return "{}.{}({})".format(T.__module__, T.__qualname__, self._values)
 
--- tests/fin/test_model.py
+++ tests/fin/test_model.py
@@ -1,11 +1,64 @@
 import unittest
 
-import fin.model.turbo as turbo
 import fin.math
+import fin.model as model
 
+# ======================================================================
+# Exceptions
+# ======================================================================
 class TestModelExceptions(unittest.TestCase):
     def test_undefined(self):
         with self.assertRaises(fin.model.Underdefined) as cm:
-            raise fin.model.Underdefined(('a','b','c'))
+            raise fin.model.Underdefined({'a':None,'b':None,'c':None}, {'a':1})
         self.assertRegex(str(cm.exception),"Too many missing parameters")
         self.assertRegex(str(cm.exception),"'a',\s*'b',\s*'c'")
+
+# ======================================================================
+# Model
+# ======================================================================
+class TestModel(unittest.TestCase):
+    def test_raises_undefined(self):
+        eq = lambda a, b, c: a + b + c
+        params = { k: None for k in eq.__code__.co_varnames}
+        values = { "a": 1 }
+
+        with self.assertRaises(fin.model.Underdefined):
+            m = model.Model(eq, params, values)
+
+    def test_generic_solver(self):
+        """
+        The generic solver should try to infer missing values
+        """
+        eq = lambda a, b, c: a + b + c
+        params = { k: None for k in eq.__code__.co_varnames}
+        values = { "a": 1, "c": 3 }
+
+        m = model.Model(eq, params, values)
+
+        self.assertEqual(m["a"], values["a"])
+        self.assertEqual(m["c"], values["c"])
+        self.assertAlmostEqual(m["b"], -4.00, delta=fin.math.EPSILON)
+
+    def test_custom_solver(self):
+        """
+        If provided, a custom solver is used to infer missing values
+        """
+        called = False
+        def solve_for_b(a,c):
+            nonlocal called
+            called = True
+
+            return -a-c
+
+        eq = lambda a, b, c: a + b + c
+        params = { k: None for k in eq.__code__.co_varnames}
+        params["b"] = solve_for_b
+        values = { "a": 1, "c": 3 }
+
+        m = model.Model(eq, params, values)
+
+        self.assertEqual(m["a"], values["a"])
+        self.assertEqual(m["c"], values["c"])
+        self.assertAlmostEqual(m["b"], -4.00, delta=fin.math.EPSILON)
+        self.assertTrue(called)
+
