de2e21e602bd5ce141deec403a327edc92383561
Sylvain Leroux <sylvain@chicoree.fr>
2023-03-16 19:52:57 +0100

    Add the Basing Point indicator

--- fin/seq/algo.py
+++ fin/seq/algo.py
@@ -5,6 +5,8 @@ import builtins
 import bisect
 from collections import deque
 
+from fin.utils.log import console
+
 # ======================================================================
 # Adapters
 # ======================================================================
@@ -270,6 +272,98 @@ def linear_trend(rowcount, values):
 
     return result
 
+# ======================================================================
+# Indicators
+# ======================================================================
+def basing_point_bull(low, high):
+    """
+    Magee's Basing Point in a bull market as explained in "Technical Analysis
+    for Stocks Trends, 11th Edition" p365.
+
+    This indicator will find Minor Bottoms from a (low, close) price pair.
+    """
+    def _basing_point_bull(rowcount, low, high):
+        MINUS_INF = float("-inf")
+        result = []
+        push = result.append
+        it = zip(range(rowcount), low, high)
+        current = None
+        highest = MINUS_INF
+        n = 0
+
+        for i, l, h in it:
+            if l is None or h is None:
+                push(current)
+                continue
+            if h > highest:
+                # Possible new high
+                high_day_of_the_lowest = highest = h
+                lowest = l
+                n = 0
+            elif l < lowest:
+                # Possible new low
+                lowest = l
+                high_day_of_the_lowest = h
+                n = 0
+            elif l > high_day_of_the_lowest:
+                # One periode completly outside the lowest day
+                n += 1
+
+            if n > 2:
+                # New Basing Point found. Reset.
+                current = lowest
+                highest = MINUS_INF
+
+            push(current)
+
+        return result
+
+    return _basing_point_bull, low, high
+
+def basing_point_bear(low, high):
+    """
+    Magee's Basing Point in a bear market as explained in "Technical Analysis
+    for Stocks Trends, 11th Edition" p365.
+
+    This indicator will find Minor Tops from a (low, close) price pair.
+    """
+    def _basing_point_bear(rowcount, low, high):
+        PLUS_INF = float("inf")
+        result = []
+        push = result.append
+        it = zip(range(rowcount), low, high)
+        current = None
+        lowest = PLUS_INF
+        n = 0
+
+        for i, l, h in it:
+            if l is None or h is None:
+                push(current)
+                continue
+            if l < lowest:
+                # Possible new low
+                low_day_of_the_highest = lowest = l
+                highest = h
+                n = 0
+            elif h > highest:
+                # Possible new low
+                highest = h
+                low_day_of_the_highest = l
+                n = 0
+            elif h < low_day_of_the_highest:
+                # One periode completly outside the highest day
+                n += 1
+
+            if n > 2:
+                # New Basing Point found. Reset.
+                current = highest
+                lowest = PLUS_INF
+
+            push(current)
+
+        return result
+
+    return _basing_point_bear, low, high
 
 # ======================================================================
 # Core functions
