46c7c97f5134fba6c5e8a42456761f5d48dc8fa6
Sylvain Leroux <sylvain@chicoree.fr>
2024-02-19 14:01:01 +0100

    Add model for the simple binomial tree

--- fin/model/option.py
+++ fin/model/option.py
@@ -48,5 +48,31 @@ def put(k, t, s_0, sigma_0, r_0, p_0, *, sqrt=math.sqrt, log=math.log, exp=math.
 
     return p_0+(s_0*cdf(-d1)-k*exp(-r_0*t)*cdf(-d2))
 
+def simple_binomial_tree_call(k, t, s_0, s_u, s_d, r_0, f_n, *, exp=math.exp):
+    """
+    Model for option pricing using a simple (one step) binomial tree.
+
+    This function evaluates to 0 if the model is at equilibrium given:
+    - k: Strike price
+    - t: Time until expiration
+    - s_0: Underlying asset price on the valuation date
+    - s_u: Underlying asset price at expiration if the price rises
+    - s_d: Underlying asset price at expiration if the price falls
+    - r_0: Continuously compounded risk-free interest rate on the valuation date
+    - f_0: Option price at expiration
+    """
+    c_0 = s_u - k # Must be > 0 !
+    delta = c_0 / (s_u - s_d)
+    fv = s_d*delta # future value of a portfolio of -1 call and delta shares
+    pv = fv*exp(-r_0*t) # present value of the same portfolio
+    f = delta*s_0-pv
+
+    return f-f_n
+
+class SimpleBinomialTree:
+    call = fin.model.Model(simple_binomial_tree_call, ())
+
+
+
 Call = fin.model.Model(call, ())
 Put = fin.model.Model(put, ())
