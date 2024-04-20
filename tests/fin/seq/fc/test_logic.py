873a5c45350c9dfb138f435d2e2f29bd19e5d7f4
Sylvain Leroux <sylvain@chicoree.fr>
2024-04-21 00:21:26 +0200

    Add the `fc:any()` logic function

--- tests/fin/seq/fc/test_logic.py
+++ tests/fin/seq/fc/test_logic.py
@@ -15,43 +15,72 @@ N = None
 # ======================================================================
 # Logic operators
 # ======================================================================
-class TestArithmeticOperators(unittest.TestCase):
+class TestLogicalOperators(unittest.TestCase):
+    def check(self, fct, *testcases):
+        for desc, *cols, expected in testcases:
+            cols = [ Column.from_sequence(col, type=Ternary()) for col in cols ]
+            with self.subTest(desc=desc):
+                ser = Serie.create(Column.from_sequence(range(len(expected))))
+                res = fct(ser, *cols)
+
+                self.assertIsInstance(res, Column)
+                self.assertSequenceEqual(res, expected)
+
     def test_all(self):
-        testcases = (
-                (
-                    "0-col truth table",
-                    (T, T, T),
-                ),
-                (
-                    "1-col truth table",
-                    (T, F, N),
-                    # -------------------------
-                    (T, F, N),
-                ),
-                (
-                    "2-cols truth table",
-                    (T, F, T, T, N, F, N, N, F),
-                    (T, T, F, N, T, F, F, N, N),
-                    # -------------------------
-                    (T, F, F, N, N, F, F, N, F),
-                ),
-                (
-                    "3-cols truth table",
+        self.check(logic.all,
+            (
+                "0-col truth table",
+                (T, T, T),
+            ),
+            (
+                "1-col truth table",
+                (T, F, N),
+                # -------------------------
+                (T, F, N),
+            ),
+            (
+                "2-cols truth table",
+                (T, F, T, T, N, F, N, N, F),
+                (T, T, F, N, T, F, F, N, N),
+                # -------------------------
+                (T, F, F, N, N, F, F, N, F),
+            ),
+            (
+                "3-cols truth table",
             (T, T, T, T, T, T, T, T, T, F, F, F, F, F, F, F, F, F, N, N, N, N, N, N, N, N, N),
             (T, T, T, F, F, F, N, N, N, T, T, T, F, F, F, N, N, N, T, T, T, F, F, F, N, N, N),
             (T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N),
                     # -------------------------
             (T, F, N, F, F, F, N, F, N, F, F, F, F, F, F, F, F, F, N, F, N, F, F, F, N, F, N),
-                ),
-            )
+            ),
+        )
 
-        for desc, *cols, expected in testcases:
-            cols = [ Column.from_sequence(col, type=Ternary()) for col in cols ]
-            with self.subTest(desc=desc):
-                ser = Serie.create(Column.from_sequence(range(len(expected))))
-                fct = logic.all
-                res = fct(ser, *cols)
-
-                self.assertIsInstance(res, Column)
-                self.assertSequenceEqual(res, expected)
+    def test_any(self):
+        self.check(logic.any,
+            (
+                "0-col truth table",
+                (F, F, F),
+            ),
+            (
+                "1-col truth table",
+                (T, F, N),
+                # -------------------------
+                (T, F, N),
+            ),
+            (
+                "2-cols truth table",
+                (T, F, T, T, N, F, N, N, F),
+                (T, T, F, N, T, F, F, N, N),
+                # -------------------------
+                (T, T, T, T, T, F, N, N, N),
+            ),
+            (
+                "3-cols truth table",
+            (T, T, T, T, T, T, T, T, T, F, F, F, F, F, F, F, F, F, N, N, N, N, N, N, N, N, N),
+            (T, T, T, F, F, F, N, N, N, T, T, T, F, F, F, N, N, N, T, T, T, F, F, F, N, N, N),
+            (T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N, T, F, N),
+                    # -------------------------
+            (T, T, T, T, T, T, T, T, T, T, T, T, T, F, N, T, N, N, T, T, T, T, N, N, T, N, N),
+            ),
+        )
 
