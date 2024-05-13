7e8c0f6ea91a02d45027fcea14d0f21bb7e82f8d
Sylvain Leroux <sylvain@chicoree.fr>
2024-05-13 14:10:23 +0200

    Improve `fin.seq.presentation` tests

--- tests/fin/seq/test_presentation.py
+++ tests/fin/seq/test_presentation.py
@@ -1,24 +1,62 @@
 import unittest
 
-from fin.seq import serie
+from fin.seq.serie import Serie
 from fin.seq import presentation
 from fin.seq import fc
 
-class TestPresentation(unittest.TestCase):
-    def test_create_one_column_presentation(self):
-        pres = presentation.Presentation(
-                heading=False,
-            )
-        ser = serie.Serie.create(fc.sequence("ABCDEF"), fc.sequence((1,2,3,4,5,6)))
-        
-        expected = "\n".join((
-            "A | 1",
-            "B | 2",
-            "C | 3",
-            "D | 4",
-            "E | 5",
-            "F | 6",
-        ))
+from tests.fin.seq.utils import ColumnFactory
 
-        self.assertEqual(pres(ser), expected)
+class TestPresentation(unittest.TestCase):
+    def test_presentation(self):
+        test_cases = (
+                "#0 Serie with one sequence column",
+                Serie.create(
+                    ColumnFactory.column_from_sequence(*"ABCDEF"),
+                    ColumnFactory.column_from_sequence(1,2,3,4,5,6),
+                ),
+                "\n".join((
+                    "A | 1",
+                    "B | 2",
+                    "C | 3",
+                    "D | 4",
+                    "E | 5",
+                    "F | 6",
+                )),
+
+                "#1 Serie with one float column",
+                Serie.create(
+                    ColumnFactory.column_from_sequence(*"ABCDEF"),
+                    ColumnFactory.column_from_float(1,2,3,4,5,6),
+                ),
+                "\n".join((
+                    "A | 1.00",
+                    "B | 2.00",
+                    "C | 3.00",
+                    "D | 4.00",
+                    "E | 5.00",
+                    "F | 6.00",
+                )),
+
+                "#2 Serie with one ternary column",
+                Serie.create(
+                    ColumnFactory.column_from_sequence(*"ABCDEF"),
+                    ColumnFactory.column_from_ternary(True, False, None, None, False, True),
+                ),
+                "\n".join((
+                    "A |  True",
+                    "B | False",
+                    "C |  None",
+                    "D |  None",
+                    "E | False",
+                    "F |  True",
+                )),
+            )
+
+        while test_cases:
+            desc, ser, expected, *test_cases = test_cases
+            with self.subTest(desc=desc):
+                pres = presentation.Presentation(
+                        heading=False,
+                    )
+                self.assertEqual(pres(ser), expected)
 
