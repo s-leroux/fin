2cc58d586718936ff5cbf67d7ed75ff51b9bbba9
Sylvain Leroux <sylvain@chicoree.fr>
2024-05-23 18:24:33 +0200

    Add interface to rerieve data from scraped tables

--- fin/scrap/table.py
+++ fin/scrap/table.py
@@ -1,4 +1,96 @@
+import sys
+
 from bs4 import BeautifulSoup
+
+def _union(selection, start, end):
+    """
+    Add all values in the (start, end) range to the selection.
+
+    This functions follows Python semantic of considering _start_ included,
+    but _end_ excluded in the ranges.
+    """
+    result = []
+    for a,b in selection:
+        if start > b:
+            result.append((a,b))
+        elif end < a:
+            result.append((start,end))
+            start = a
+            end = b
+        elif start <= a:
+            end = max(end, b)
+        elif end >= b:
+            start = min(start, a)
+        else:
+            start = a
+            end = b
+
+    result.append((start,end))
+
+    return result
+
+def cols(start, end):
+    def _cols(cols, rows):
+        return _union(cols, start, end), rows
+
+    return _cols
+
+def rows(start, end):
+    def _rows(cols, rows):
+        return cols, _union(rows, start, end)
+
+    return _rows
+
+class Table:
+    def __init__(self, indices, data, *, data_row_start=0, data_col_start=0):
+        self.data = data
+        self.indices = indices
+        self.data_row_start = data_row_start
+        self.data_col_start = data_col_start
+
+    def __repr__(self):
+        return f"Table({self.indices!r}, {self.data!r}"
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def select(self, *headings):
+        selection = ((),())
+        for heading in headings:
+            selection = self.indices[heading](*selection)
+
+        return selection
+
+    def find(self, *headings):
+        cols, rows = self.select(*headings)
+        if not cols:
+            cols = ((self.data_col_start, sys.maxsize),)
+        if not rows:
+            rows = ((self.data_row_start, sys.maxsize),)
+
+        result = []
+        for rstart, rend in rows:
+            for rindex in range(rstart, rend):
+                try:
+                    in_row = self.data[rindex]
+                except IndexError:
+                    break
+
+                out_row = []
+                for cstart, cend in cols:
+                    for cindex in range(cstart, cend):
+                        try:
+                            cell = in_row[cindex]
+                        except IndexError:
+                            break
+
+                        out_row.append(cell)
+                result.append(out_row)
+
+        return result
 
 def parse(fragment):
     """
@@ -8,14 +100,31 @@ def parse(fragment):
     if tbody is None:
         tbody = fragment
 
+    indices = {}
+    data = []
+    ncolindices = 0
 
-    py_body = []
-    for tr in tbody.find_all("tr"):
+    data_row_start = 0
+    data_col_start = 0
+
+    for rnum, tr in enumerate(tbody.find_all("tr")):
         py_row = []
-        for titem in tr.find_all(("th", "td")):
-            py_row.append(titem.get_text(strip=True))
+        for cnum, titem in enumerate(tr.find_all(("th", "td"))):
+            text = titem.get_text(strip=True)
+            py_row.append(text)
+            if titem.name == "th":
+                if cnum > 0:
+                    indices[text] = cols(cnum, cnum+1)
+                    ncolindices += 1
+                else:
+                    rows(rnum, rnum+1)
+            elif cnum == 0 and ncolindices == 0:
+                # Heuristic: the first td element of each row is assumed to
+                # be a row header if there was no column headers.
+                indices[text] = rows(rnum, rnum+1)
+                data_col_start = 1
 
-        py_body.append(py_row)
+        data.append(py_row)
 
 
-    return py_body
+    return Table(indices, data, data_row_start=data_row_start, data_col_start=data_col_start)
--- tests/fin/scrap/test_table.py
+++ tests/fin/scrap/test_table.py
@@ -2,7 +2,86 @@ import unittest
 
 from fin.scrap import table
 
+# Private functions
+from fin.scrap.table import _union
+
 from bs4 import BeautifulSoup
+
+class TestTableUtils(unittest.TestCase):
+    def test_union(self):
+        test_cases = (
+                "#0 empty",
+                [],
+                (5, 10),
+                [(5, 10)],
+
+                "#1.0 insert before stricly",
+                [(5, 10)],
+                (1, 3),
+                [(1, 3), (5, 10)],
+
+                "#1.1 insert before touch",
+                [(5, 10)],
+                (1, 5),
+                [(1, 10)],
+
+                "#1.2 insert before intersect",
+                [(5, 10)],
+                (1, 8),
+                [(1, 10)],
+
+                "#2.0 insert after stricly",
+                [(5, 10)],
+                (15, 20),
+                [(5, 10), (15, 20)],
+
+                "#2.1 insert after touch",
+                [(5, 10)],
+                (10, 20),
+                [(5, 20)],
+
+                "#2.2 insert after intersect",
+                [(5, 10)],
+                (8, 20),
+                [(5, 20)],
+
+                "#3 insert outside",
+                [(5, 10)],
+                (1, 20),
+                [(1, 20)],
+
+                "#4 insert inside",
+                [(5, 10)],
+                (7, 9),
+                [(5, 10)],
+
+                "#5 join two segments",
+                [(5, 10), (15, 20)],
+                (10, 15),
+                [(5, 20)],
+
+                "#6 intersect two segments",
+                [(5, 10), (15, 20)],
+                (8, 18),
+                [(5, 20)],
+
+                "#7 enclose two segments",
+                [(5, 10), (15, 20)],
+                (1, 25),
+                [(1, 25)],
+
+                "#8 insert between",
+                [(5, 10), (20, 25)],
+                (15, 17),
+                [(5, 10), (15, 17), (20, 25)],
+
+            )
+
+        while test_cases:
+            desc, selection, new, expected, *test_cases = test_cases
+            with self.subTest(desc=desc):
+                actual = _union(selection, *new)
+                self.assertSequenceEqual(actual, expected)
 
 class TestTableScraper(unittest.TestCase):
     def setUp(self):
@@ -23,7 +102,17 @@ class TestTableScraper(unittest.TestCase):
 
         with open(fname, "rt") as f:
             doc = f.read()
-            soup = BeautifulSoup(doc, "html.parser")
 
-            res = table.parse(soup.table)
-            self.assertSequenceEqual(res, expected)
+        soup = BeautifulSoup(doc, "html.parser")
+
+        tbl = table.parse(soup.table)
+        self.assertSequenceEqual(tuple(tbl), expected)
+
+        for idx, (key, value) in enumerate(expected):
+            found = tbl.select(key)
+            self.assertEqual(found, ((), [(idx, idx+1)]), msg=key)
+
+        for key, value in expected:
+            found = tbl.find(key)
+            self.assertSequenceEqual(found, [[value]], msg=key)
+
