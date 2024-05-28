ac38059a74380c1d8cc6c89a5e2980377a2ef744
Sylvain Leroux <sylvain@chicoree.fr>
2024-05-28 14:08:15 +0200

    Factor out fin/rest.py

---
+++ fin/rest.py
@@ -0,0 +1,96 @@
+import os
+import re
+
+from fin import requests
+
+class RestAPI:
+    """
+    Helper class to access REST APIs.
+    """
+    def __init__(self, base_url, api_key=None, *, api_key_param = "apikey"):
+        self._base_url = base_url
+        self._api_key = api_key
+        self._api_key_param = api_key_param
+
+    def url_for_endpoint(self, endpoint, path):
+        url = endpoint
+        base_url = self._base_url
+
+        if not endpoint.startswith(base_url):
+            url = f"{base_url}/{endpoint}"
+
+        if path:
+            url = f"{url}/{path}"
+
+        return url
+
+    def adjust_params(self, params):
+        """
+        Add API specific parameters to the request.
+
+        Mostly intended to inject the API key into the request.
+        """
+        params = params.copy()
+        api_key = self._api_key_param
+        if api_key is not None:
+            params[self._api_key_param] = self._api_key
+
+        return params
+    
+    def _get(self, endpoint, path=None, *, params={}, options={}):
+        """
+        Send a GET request to the given API endpoint.
+        """
+        _get = options.get("get", requests.get)
+
+        url = self.url_for_endpoint(endpoint, path)
+        params = self.adjust_params(params)
+
+        return _get(url, params=params)
+
+def _get_wrapper(endpoint, param_list):
+    # XXX This should add a layer of parameter checking
+    def get(self, *args, **kwargs):
+        return self._get(endpoint, *args, params=kwargs)
+
+    return get
+
+MANDATORY = True
+OPTIONAL = False
+
+VERSION_PREFIX_RE = re.compile("^v[0-9]+/")
+
+class RestAPIBuilder:
+    def __init__(self, class_name, *, api_base_class = RestAPI):
+        self._class_name = class_name
+        self._api_base_class = api_base_class
+        self._methods = {}
+
+    def __call__(self):
+        methods = {
+            name: method for name, method in self._methods.values()
+        }
+        return type(self._class_name, (self._api_base_class,), methods)
+
+    def method_name_for_endpoint(self, endpoint):
+        endpoint = VERSION_PREFIX_RE.sub("", endpoint)
+        endpoint = endpoint.replace("-","_")
+        return endpoint
+
+    def register(self, endpoint, verb, param_list=None, *, method_name=None):
+        if not method_name:
+            method_name = self.method_name_for_endpoint(endpoint)
+
+        if verb == "get":
+            wrapper = _get_wrapper
+        else:
+            raise NotImplementedError(f"RestAPIBuildr.register is not implemented for the verb {verb!r}")
+
+        method = (
+                method_name,
+                wrapper(endpoint, param_list)
+            )
+
+        self._methods[method_name] = method
+        return method
+
--- tests/fin/api/test_fmp.py
+++ tests/fin/api/test_fmp.py
@@ -1,94 +1,10 @@
 import unittest
 import os
 
-from fin.api.fmp import RestAPI, RestAPIBuilder, FMPRestAPI
-
-HTTPBIN_BASE_URL = "https://httpbingo.org"
+from fin.api.fmp import FMPRestAPI
 
 SLOW_TESTS = os.environ.get("SLOW_TESTS")
 FMP_API_KEY = os.environ.get("FMP_API_KEY")
-
-class TestRestAPI(unittest.TestCase):
-    def test_get(self):
-        request_args = ()
-        request_kwargs = {}
-        def fake_get(*args, **kwargs):
-            return (args, kwargs)
-
-        api_key = "myapikey"
-        endpoint = "json"
-
-        api = RestAPI(HTTPBIN_BASE_URL, api_key)
-        request_args, request_kwargs = api._get(endpoint,
-            params=dict(
-                query="AA",
-            ),
-            options=dict(
-                get=fake_get,
-            )
-        )
-
-        self.assertSequenceEqual(request_args, (
-            f"{HTTPBIN_BASE_URL}/{endpoint}",
-        ))
-        self.assertEqual(request_kwargs["params"], {
-            "apikey": api_key,
-            "query": "AA",
-        })
-
-class TestRestAPIBuilder(unittest.TestCase):
-    def test_register(self):
-        test_cases = (
-            "#0",
-            "my_endpoint",
-            "my_endpoint",
-
-            "#1 Remove version prefix",
-            "v3/search",
-            "search",
-
-            "#2 replace '-' by '_'",
-            "v3/search-name",
-            "search_name",
-        )
-
-        while test_cases:
-            desc, endpoint, method, *test_cases = test_cases
-            with self.subTest(desc=desc):
-                builder = RestAPIBuilder("MyAPIClass")
-                builder.register(endpoint, "get")
-
-                self.assertIn(method, builder._methods)
-
-                api = builder()
-
-                self.assertIn(method, dir(api))
-
-class TestRestAPIBuilderBuilt(unittest.TestCase):
-    if SLOW_TESTS:
-        def setUp(self):
-            self.builder = RestAPIBuilder("MyAPIClass")
-
-        def test_get(self):
-            self.builder.register("range", "get")
-            api_class = self.builder()
-            api = api_class(HTTPBIN_BASE_URL)
-
-            result = api.range("26")
-            self.assertEqual(result.status_code, 200)
-            self.assertEqual(result.text, "abcdefghijklmnopqrstuvwxyz")
-
-        def test_get_with_param(self):
-            self.builder.register("get", "get", {
-                "x": ( True, int),
-            })
-            api_class = self.builder()
-            api = api_class(HTTPBIN_BASE_URL)
-
-            result = api.get(x="26")
-            self.assertEqual(result.status_code, 200)
-            json = result.json()
-            self.assertSequenceEqual(json["args"]["x"], ["26"])
 
 class TestFMPApi(unittest.TestCase):
     if SLOW_TESTS and FMP_API_KEY:
