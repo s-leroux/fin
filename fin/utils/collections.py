ca41ea0ce983ee1161bc82dc3c2ecd8f423e1b4a
Sylvain Leroux <sylvain@chicoree.fr>
2024-06-01 09:58:25 +0200

    Add mapping/mapping -> list/list conversions utility

--- fin/utils/collections.py
+++ fin/utils/collections.py
@@ -6,5 +6,23 @@ def dictionaries_to_list(fields, iterable):
     """
     Convert dictionaries to list of values.
     """
-    for obj in iterable:
-        yield [obj[field] for field in fields]
+    for mapping in iterable:
+        yield [mapping[field] for field in fields]
+
+def km_to_ll(fields, iterable):
+    """
+    Convert an interable of (key, values) pairs, where values is
+    a mapping, to a nested list.
+
+    This is useful when a collections is (somewhat improperly)
+    stored in a mappping instead of a sequence.
+    """
+    for key, mapping in iterable:
+        yield [key, *[mapping[field] for field in fields]]
+
+def mm_to_ll(fields, mapping):
+    """
+    Convert a mapping of mappings to a list of lists.
+    """
+    return km_to_ll(fields, mapping.items())
+
--- tests/fin/utils/test_collections.py
+++ tests/fin/utils/test_collections.py
@@ -41,3 +41,63 @@ class TestDictionariesToList(unittest.TestCase):
             with self.subTest(desc=desc):
                 actual = list(uc.dictionaries_to_list(fields, dictionaries))
                 self.assertEqual(actual, expected)
+
+class TestKMToLL(unittest.TestCase):
+    def test_it(self):
+        test_cases = (
+            "#0 Base case",
+            ("x", "y"),
+            {
+                "B": { "x": 20, "y": 200 },
+                "A": { "x": 10, "y": 100 },
+                "C": { "x": 30, "y": 300 },
+            },
+            [
+                [ "A", 10, 100 ],
+                [ "B", 20, 200 ],
+                [ "C", 30, 300 ],
+            ],
+        )
+
+        while test_cases:
+            desc, fields, mm, expected, *test_cases = test_cases
+            with self.subTest(desc=desc):
+                actual = list(uc.km_to_ll(fields, sorted(mm.items())))
+                self.assertEqual(actual, expected)
+
+class TestMMToLL(unittest.TestCase):
+    def test_it(self):
+        test_cases = (
+            "#0 Base case",
+            ("x", "y"),
+            { # This test assumes Python 3.7 ordered dicts feature (should work on CPython 3.6 too)
+                "A": { "x": 10, "y": 100 },
+                "B": { "x": 20, "y": 200 },
+                "C": { "x": 30, "y": 300 },
+            },
+            [
+                [ "A", 10, 100 ],
+                [ "B", 20, 200 ],
+                [ "C", 30, 300 ],
+            ],
+
+            "#1 Order preserving",
+            ("x", "y"),
+            { # This test assumes Python 3.7 ordered dicts feature (should work on CPython 3.6 too)
+                "B": { "x": 20, "y": 200 },
+                "A": { "x": 10, "y": 100 },
+                "C": { "x": 30, "y": 300 },
+            },
+            [
+                [ "B", 20, 200 ],
+                [ "A", 10, 100 ],
+                [ "C", 30, 300 ],
+            ],
+        )
+
+        while test_cases:
+            desc, fields, mm, expected, *test_cases = test_cases
+            with self.subTest(desc=desc):
+                actual = list(uc.mm_to_ll(fields, mm))
+                self.assertEqual(actual, expected)
+
